d3029830cc3c9eac5200060d00835e34076f619c
Sergei Legkodymov <rutven@gmail.com>
2021-01-31 20:09:41 +0300

    added fruits tests

--- src/main/java/name/legkodymov/hotel/FruitResource.java
+++ src/main/java/name/legkodymov/hotel/FruitResource.java
@@ -1,11 +1,8 @@
 package name.legkodymov.hotel;
 
-import name.legkodymov.hotel.model.Fruit;
-
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Set;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -13,13 +10,15 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import name.legkodymov.hotel.model.Fruit;
 
 @Path("/fruits")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class FruitResource {
 
-    private Set<Fruit> fruits = Collections.newSetFromMap(Collections.synchronizedMap(new LinkedHashMap<>()));
+    private final Set<Fruit> fruits = Collections
+        .newSetFromMap(Collections.synchronizedMap(new LinkedHashMap<>()));
 
     public FruitResource() {
         fruits.add(new Fruit("Apple", "Winter fruit"));
